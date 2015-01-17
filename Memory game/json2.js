0d0733118268a4cd0a454067d335e3c3e8e4e6f3
mohamed.meshref2020@yahoo.com <mohamed.meshref2020@yahoo.com>
2015-01-17 13:28:07 +0200

    first

---
+++ Memory game/json2.js
@@ -0,0 +1,55 @@
+<Dept name="Open Source WD">
+	<GraduateDomain>
+		<JobTile>PHP web Weveloper</JobTile>
+		<JobTile>Python  Developer</JobTile>
+		<JobTile>Perl  Developer</JobTile>
+		<JobTile>Ruby  and  Rails  Developer</JobTile>
+		<JobTile>OpenERP - Odoo Developer</JobTile>
+	</GraduateDomain>
+	<ListOfCourses>
+		<Categories>
+			<Category Name="Front-End Technologies">
+				<Courses No="3">
+					<Course>
+						<Name>JavaScript</Name>
+						<Hours>24</Hours>
+					</Course>
+					<Course>
+						<Name>HTML</Name>
+						<Hours>18</Hours>
+					</Course>
+					<Course>
+						<Name>XML  and  Related  Technologies</Name>
+						<Hours>18</Hours>
+					</Course>
+				</Courses>
+			</Category>
+			
+			<Category Name="Back-End  Technologies">
+				<Courses No="2">
+					<Course>
+						<Name>PHP  Programming </Name>
+						<Hours>30</Hours>
+					</Course>
+					<Course>
+						<Name>PHP  CMSs</Name>
+						<Hours>24</Hours>
+					</Course>
+				</Courses>
+			</Category>
+			
+			<Category Name="Database Management Systems">
+				<Courses No="2">
+					<Course>
+						<Name>MySQL </Name>
+						<Hours>30</Hours>
+					</Course>
+					<Course>
+						<Name>MongoDB</Name>
+						<Hours>24</Hours>
+					</Course>
+				</Courses>
+			</Category>
+		</Categories>
+	<ListOfCourses>
+</Dept>
